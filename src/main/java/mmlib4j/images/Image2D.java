2e74bc9c58d339cd03675a7df15486eab563b62a
Renã Passos de Souza <ren.boxe@gmail.com>
2015-11-18 22:34:09 -0200

    Alterado estrutura de diretórios

---
+++ src/main/java/mmlib4j/images/Image2D.java
@@ -0,0 +1,38 @@
+package mmlib4j.images;
+
+import mmlib4j.images.impl.PixelIndexer;
+
+/**
+ * MMLib4J - Mathematical Morphology Library for Java 
+ * @author Wonder Alexandre Luz Alves
+ *
+ */
+public interface Image2D {
+    
+    public int getHeight();
+    
+    public int getWidth();
+    
+    public int getSize();
+    
+    public boolean isPixelValid(int x, int y);
+
+    public boolean isPixelValid(int p);
+
+    public Iterable<Integer> scanForward();
+   
+    public Iterable<Integer> scanBackward();
+    
+    public int convertToIndex(int x, int y);
+    
+    public int getDepth();
+   
+    public PixelIndexer getPixelIndexer();
+    
+    public void setPixelIndexer(PixelIndexer indexer);
+    
+    public int getIndex(int p);
+    
+    public int getIndex(int x, int y);
+}
+
